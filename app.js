540cf87a9e1da8dafc109eaa9b7d73d6a820fa38
Jason Corbett <jsncbt@gmail.com>
2020-05-11 12:02:58 +1000

    Latest changes

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const night = document.getElementById('night');
 beach.style.height = window.outerHeight + 'px';
 
 const handleScroll = () => raf( () => {
-    console.log(Math.floor(window.scrollY));
     const scrolled = Math.floor(window.scrollY);
-    logo.style.transform = 'translateY(' + scrolled + 'px)';
+
+    // Start going dusk
     if (scrolled > 100) {
+      logo.classList.add('logo--hide');
       night.style.opacity = 1;
     } else {
+      logo.classList.remove('logo--hide');
       night.style.opacity = 0;
     }
   })
@@ -24,4 +26,54 @@ const raf =
     window.setTimeout( callback, 1000 / 60 )
   }
 
-window.addEventListener('scroll', handleScroll);
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
+
+window.addEventListener("DOMContentLoaded", function() {
+
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("form");
+  var button = document.getElementById("form-button");
+  var status = document.getElementById("form-status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    button.style = "display: none ";
+    status.innerHTML = "Thanks!";
+  }
+
+  function error() {
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+
+
+  window.addEventListener('scroll', handleScroll);
+});
+
